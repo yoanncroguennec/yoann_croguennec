c8b65aa4629b19df65f1188a1b09e03b22f37747
sido69 <yoann.croguennec@gmail.com>
2025-03-12 18:34:49 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Restaurant Finder",
+  description: "Find the best restaurants near you",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.className} antialiased`}
+        cz-shortcut-listen="true"
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
