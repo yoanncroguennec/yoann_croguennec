003a825603df1ef63d8ce313db483d45caae8aa0
sido69 <yoann.croguennec@gmail.com>
2025-05-26 18:25:17 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import ResumePage from "./screens/pdf/ResumePage";
 import SocialsLinks from "./screens/SocialsLinks";
 
 export default function page() {
-  // return (
-  //   <div style={{ width: "100%", height: "100vh" }}>
-  //     <iframe
-  //       src="/docs/2025_01_23_Update_CV.pdf"
-  //       width="100%"
-  //       height="100%"
-  //       style={{ border: "none" }}
-  //     />
-  //   </div>
-  // );
-  // // return <ResumePage />
   return <SocialsLinks />;
 }
